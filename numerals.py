be83f8a87ede431a63e16edad18dde49570aba3c
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2026-03-17 12:12:57 +0200

    Реализация на наредени двойки и примитивно рекурсивни функции

--- numerals.py
+++ numerals.py
@@ -32,3 +32,28 @@ cFalse = lambda x: lambda y: y
 def tobool(b):
     return b(True)(False)
 
+cnot = lambda b: lambda x: lambda y: b(y)(x)
+cnott = lambda b: b(cFalse)(cTrue)
+
+cand = lambda p: lambda q: p(q)(cFalse)
+cor = lambda p: lambda q: p(cTrue)(q)
+
+ciszero = lambda n: n(lambda b: cFalse)(cTrue)
+ceven = lambda n: n(cnot)(cTrue)
+
+cpair = lambda x: lambda y: lambda z: z(x)(y)
+cleft = lambda p: p(cTrue)
+cright = lambda p: p(cFalse)
+
+def tointpair(p):
+    return (toint(cleft(p)), toint(cright(p)))
+
+cpstep = lambda p: cpair(cs(cleft(p)))(cleft(p))
+
+cp = lambda n: cright(n(cpstep)(cpair(c0)(c0)))
+
+cfactstep = lambda p: \
+              cpair(cs(cleft(p))) \
+                   (cmult(cs(cleft(p)))(cright(p)))
+
+cfact = lambda n: cright(n(cfactstep)(cpair(c0)(c1)))
