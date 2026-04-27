222396ddec02acb75dcfb966379c2592497e7e8c
Trifon Trifonov <triffon@gmail.com>
2026-04-27 21:18:15 +0300

    Пример за полиморфни термове от система F в Haskell

---
+++ SystemF.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
+
+-- applyToBoth :: (forall a. (a -> a)) -> (Int, Bool) -> (Int, Bool)
+applyToBoth f (x, y) = (f x, f y)
+
+
+{-
+applyToBoth :: forall a. (a -> a) -> (Int, Bool) -> (Int, Bool)
+applyToBoth f (x, y) = (f x, f y)
+-}
+
+-- applyToBoth id (42, True) ---> (42, True)
