6e21cf7f79131bbaa84ad91c02d6378bc999ee5a
Trifon Trifonov <ttrifonov@asteasolutions.com>
2026-04-30 14:05:17 +0300

    Още един пример за система F в Haskell

--- SystemF.hs
+++ SystemF.hs
@@ -10,3 +10,10 @@ applyToBoth f (x, y) = (f x, f y)
 -}
 
 -- applyToBoth id (42, True) ---> (42, True)
+
+pickFromMatrix :: (forall a. ([a] -> a)) -> [[a]] -> a
+pickFromMatrix f l = f (f l)
+
+-- pickFromMatrix head  [[1,2,3],[4,5,6],[7,8,9]] ---> 1
+-- pickFromMatrix last  [[1,2,3],[4,5,6],[7,8,9]] ---> 9
+-- pickFromMatrix (!!1) [[1,2,3],[4,5,6],[7,8,9]] ---> 5
